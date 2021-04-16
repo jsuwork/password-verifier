80265767e2893f86a58b01c69fd2f361dfb934b4
John Su <johnsu.work@outlook.com>
2021-04-16 23:50:43 +0100

    Define test item 2 - Password must not be null

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -49,3 +49,81 @@ describe("Item 1 - Password must be longer than 8 characters", () => {
     }
   });
 });
+
+describe("Item 2 - Password must not be null", () => {
+  it("fails criteria when password is undefined", () => {
+    try {
+      validatePassword(undefined, true);
+    } catch (e) {
+      expect(e).toEqual(jasmine.any(PasswordValidationError));
+      let hasRelevantError = false;
+      for (let error of e.errors) {
+        if (error instanceof PasswordNullError) {
+          hasRelevantError = true;
+          break;
+        }
+      }
+      expect(hasRelevantError).toBe(true);
+    }
+  });
+  it("fails criteria when password is null", () => {
+    try {
+      validatePassword(null, true);
+    } catch (e) {
+      expect(e).toEqual(jasmine.any(PasswordValidationError));
+      let hasRelevantError = false;
+      for (let error of e.errors) {
+        if (error instanceof PasswordNullError) {
+          hasRelevantError = true;
+          break;
+        }
+      }
+      expect(hasRelevantError).toBe(true);
+    }
+  });
+  it("fails criteria when password is not a string", () => {
+    try {
+      validatePassword(12345, true);
+    } catch (e) {
+      expect(e).toEqual(jasmine.any(PasswordValidationError));
+      let hasRelevantError = false;
+      for (let error of e.errors) {
+        if (error instanceof PasswordNullError) {
+          hasRelevantError = true;
+          break;
+        }
+      }
+      expect(hasRelevantError).toBe(true);
+    }
+  });
+  it("passes criteria when password is a string", () => {
+    // With a normal string not initialised using 'new String("")'
+    try {
+      validatePassword("12345", true);
+    } catch (e) {
+      expect(e).toEqual(jasmine.any(PasswordValidationError));
+      let stillHasSpecificError = false;
+      for (let error of e.errors) {
+        if (error instanceof PasswordNullError) {
+          stillHasSpecificError = true;
+          break;
+        }
+      }
+      expect(stillHasSpecificError).toBe(false);
+    }
+    // With a String initialised using 'new String("")'
+    try {
+      validatePassword(new String("12345"), true);
+    } catch (e) {
+      expect(e).toEqual(jasmine.any(PasswordValidationError));
+      let stillHasSpecificError = false;
+      for (let error of e.errors) {
+        if (error instanceof PasswordNullError) {
+          stillHasSpecificError = true;
+          break;
+        }
+      }
+      expect(stillHasSpecificError).toBe(false);
+    }
+  });
+});
